7ebfff53989a0a45a4e85b4e22eb008b255af216
Max Schik <max.schik@googlemail.com>
2024-03-17 00:33:25 +0000

    fixes seed in augmentation

--- faf/data/augmentation.py
+++ faf/data/augmentation.py
@@ -5,41 +5,38 @@ import albumentations as A
 
 
 class Augmentation:
-    def __init__(self,
-                 crop_p=0.5,
-                 rotate_p=1,
-                 h_flip_p=0.5,
-                 v_flip_p=0.5,
-                 contrast_p=0.2,
-                 seed=42):
+    def __init__(
+        self,
+        crop_p=0.5,
+        rotate_p=0.3,
+        h_flip_p=0.5,
+        v_flip_p=0.5,
+        contrast_p=0.2,
+        seed=42,
+    ):
         self.crop_p = crop_p
         self.rotate_p = rotate_p
         self.h_flip_p = h_flip_p
         self.v_flip_p = v_flip_p
         self.contrast_p = contrast_p
         self.seed = seed
+        random.seed(self.seed)
 
-    def __call__(self,
-                 width: int,
-                 height: int,
-                 image: torch.Tensor,
-                 targets: torch.Tensor):
+    def __call__(
+        self, width: int, height: int, image: torch.Tensor, targets: torch.Tensor
+    ):
         transform = A.Compose(
             [
                 A.RandomCrop(
-                    height=int(height * 0.75), width=int(width * 0.75),
-                    p=self.crop_p
+                    height=int(height * 0.75), width=int(width * 0.75), p=self.crop_p
                 ),
                 A.RandomRotate90(p=self.rotate_p),
                 A.HorizontalFlip(p=self.h_flip_p),
                 A.VerticalFlip(p=self.v_flip_p),
                 A.RandomBrightnessContrast(p=self.contrast_p),
-            ], bbox_params=A.BboxParams(format='yolo',
-                                        min_area=100,
-                                        min_visibility=0.1)
+            ],
+            bbox_params=A.BboxParams(format="yolo", min_area=100, min_visibility=0.1),
         )
-
-        random.seed(self.seed)
 
         num_boxes = len(targets)
         # Keep only bounding boxes that are non-zero
@@ -50,11 +47,11 @@ class Augmentation:
         targets = targets[:first_zero_row]
 
         transformed = transform(image=image, bboxes=targets)
-        image = transformed['image']
+        image = transformed["image"]
 
         bboxes = torch.Tensor()
-        if transformed['bboxes']:
-            bboxes = torch.stack([torch.Tensor(i) for i in transformed['bboxes']])
+        if transformed["bboxes"]:
+            bboxes = torch.stack([torch.Tensor(i) for i in transformed["bboxes"]])
 
         padding = num_boxes - len(bboxes)
         pad_tensor = torch.zeros((padding, 6))
